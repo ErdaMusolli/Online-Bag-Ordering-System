f78a38038e22f0bc44d3c4aae576f6140b4d825a
ErdaMusolli <erdamusolli@gmail.com>
2025-05-11 16:27:28 +0200

    Frontend + Backend – User Login (React + .NET)

--- Corta/corta-frontend/src/App.jsx
+++ Corta/corta-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import TestBootstrap from './components/TestBootstrap';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TestBootstrap />} /> 
         <Route path="/register" element={<Register />} /> 
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
