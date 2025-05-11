f59559ae3eac07311240479706e2eb9c507a1592
Vlerashabiu <vs70105@ubt-uni.net>
2025-05-11 13:15:30 +0200

    Perditesim ne lidhjen me databaze

---
+++ Corta/Migrations/20250511111259_CreateNewsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Corta.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
